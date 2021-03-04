dc867651d66f3473be4c83d2e40259c033a3e128
Asus <Asus@DESKTOP-N4JIJJG>
2021-03-04 10:23:00 +0200

    Al doilea push seminar 2

--- CTS_Seminar2/src/ro/ase/cts/program/Program.java
+++ CTS_Seminar2/src/ro/ase/cts/program/Program.java
@@ -1,17 +1,18 @@
 package ro.ase.cts.program;
 
 import java.io.FileNotFoundException;
+
 import java.util.List;
 
 import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.clase.Utils;
+import ro.ase.cts.readers.AngajatReader;
 
 public class Program {
 
 	public static void main(String[] args) {
 		List<Angajat> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
 			for(Angajat angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
